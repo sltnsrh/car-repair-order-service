bb4106a5832d81e87b279c1641d6c3b8d732747f
Serhii Salatin <lifenjoy8888@gmail.com>
2023-02-20 11:47:25 +0200

    initial commit

---
+++ src/test/java/com/salatin/orderservice/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salatin.orderservice.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
