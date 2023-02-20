0eee10fd4612ee81f1a8d5bd667f3faff38afeae
Serhii Salatin <lifenjoy8888@gmail.com>
2023-02-20 12:34:12 +0200

    were replaced models from user-service

---
+++ src/main/java/com/salatin/orderservice/model/Order.java
@@ -0,0 +1,21 @@
+package com.salatin.orderservice.model;
+
+import com.salatin.userservice.model.status.OrderStatus;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order {
+    private Long id;
+    //    @CreatedDate
+    private LocalDateTime createdAt;
+    private LocalDateTime customerRequestedAt;
+    private LocalDateTime startRepairAt;
+    private LocalDateTime finishRepairAt;
+    private LocalDateTime submittedAt;
+    private List<Part> parts;
+    private List<Work> works;
+    private LocalDateTime totalExecutionTime;
+    private BigDecimal total;
+    private OrderStatus status;
+}
---
+++ src/main/java/com/salatin/orderservice/model/Part.java
@@ -0,0 +1,8 @@
+package com.salatin.orderservice.model;
+
+public class Part {
+    private Long id;
+    private String name;
+    private String brand;
+    private Integer oemNumber;
+}
