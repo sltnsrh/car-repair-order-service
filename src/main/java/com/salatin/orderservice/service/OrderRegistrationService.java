efb1d51d09225e4e8199bde612393e8bc2a458ea
Serhii Salatin <lifenjoy8888@gmail.com>
2023-06-26 18:20:08 +0300

    in OrderRegistationService in method register logic was changed

--- src/main/java/com/salatin/orderservice/service/OrderRegistrationService.java
+++ src/main/java/com/salatin/orderservice/service/OrderRegistrationService.java
@@ -1,16 +1,21 @@
 package com.salatin.orderservice.service;
 
 import com.salatin.orderservice.model.Order;
+import com.salatin.orderservice.model.OrderStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class OrderRegistrationService {
     private final OrderService orderService;
 
@@ -20,14 +25,30 @@ public class OrderRegistrationService {
                 .anyMatch(a -> "ROLE_manager".equals(a.getAuthority()));
 
         if (hasRoleManager) {
-            if (order.getCustomerId() == null) {
-                return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        "Customer id can't be null"));
-            }
-            return orderService.create(order);
+            return registerAsManager(order, authenticationToken.getName());
         }
 
-        order.setCustomerId(authenticationToken.getName());
-        return orderService.create(order);
+        return registerAsCustomer(order, authenticationToken.getName());
+    }
+
+    private Mono<Order> registerAsManager(Order order, String managerId) {
+        if (order.getCustomerId() == null) {
+            log.warn("Customer id is null. Field 'customerId' is required");
+            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Customer id can't be null"));
+        }
+
+        order.setStatus(OrderStatus.SUBMITTED);
+        order.setManagerId(managerId);
+        order.setSubmittedAt(LocalDateTime.now());
+
+        return orderService.save(order);
+    }
+
+    private Mono<Order> registerAsCustomer(Order order, String customerId) {
+        order.setCustomerId(customerId);
+        order.setStatus(OrderStatus.CREATED);
+
+        return orderService.save(order);
     }
 }
