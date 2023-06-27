75a021499a9fb9fd3b7b640ae3321900ba2d36ca
Serhii Salatin <lifenjoy8888@gmail.com>
2023-06-27 22:38:45 +0300

    mthd register was refactored and added check if customer makes order with own car

--- src/main/java/com/salatin/orderservice/service/OrderRegistrationService.java
+++ src/main/java/com/salatin/orderservice/service/OrderRegistrationService.java
@@ -7,9 +7,11 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -23,32 +25,32 @@ public class OrderRegistrationService {
 
     public Mono<Order> register(@NotNull Order order,
                                 @NotNull JwtAuthenticationToken authenticationToken) {
-        return Mono.defer(() -> {
-            var bearerToken = "Bearer " + authenticationToken.getToken().getTokenValue();
+        var bearerToken = "Bearer " + authenticationToken.getToken().getTokenValue();
 
-            Mono<String> monoCustomerId = webClientBuilder.build().get()
-                    .uri("http://car-service/cars/{carId}", order.getCarId())
-                    .header(HttpHeaders.AUTHORIZATION, bearerToken)
-                    .retrieve()
-                    .bodyToMono(Car.class)
-                    .doOnNext(car -> {
-                        String customerId = car.getOwnerId();
-                        log.info("Retrieved customerId: {}", customerId);
-                    })
-                    .map(Car::getOwnerId);
+        Mono<String> monoCustomerId = webClientBuilder.build().get()
+                .uri("http://car-service/cars/{carId}", order.getCarId())
+                .header(HttpHeaders.AUTHORIZATION, bearerToken)
+                .retrieve()
+                .bodyToMono(Car.class)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Can't find a car with id: " + order.getCarId())))
+                .doOnNext(car -> {
+                    String customerId = car.getOwnerId();
+                    log.info("Retrieved customerId: {}", customerId);
+                })
+                .map(Car::getOwnerId);
 
-            var hasRoleManager = authenticationToken.getAuthorities().stream()
-                    .anyMatch(a -> "ROLE_manager".equals(a.getAuthority()));
+        var hasRoleManager = authenticationToken.getAuthorities().stream()
+                .anyMatch(a -> "ROLE_manager".equals(a.getAuthority()));
 
-            return monoCustomerId.flatMap(customerId -> {
-                order.setCustomerId(customerId);
+        return monoCustomerId.flatMap(customerId -> {
+            order.setCustomerId(customerId);
 
-                if (hasRoleManager) {
-                    return registerAsManager(order, authenticationToken.getName());
-                } else {
-                    return registerAsCustomer(order);
-                }
-            });
+            if (hasRoleManager) {
+                return registerAsManager(order, authenticationToken.getName());
+            } else {
+                return registerAsCustomer(order, authenticationToken.getName());
+            }
         });
     }
 
@@ -60,9 +62,13 @@ public class OrderRegistrationService {
         return orderService.save(order);
     }
 
-    private Mono<Order> registerAsCustomer(Order order) {
-        order.setStatus(OrderStatus.CREATED);
+    private Mono<Order> registerAsCustomer(Order order, String currentUserId) {
+        if (order.getCustomerId().equals(currentUserId)) {
+            order.setStatus(OrderStatus.CREATED);
+            return orderService.save(order);
+        }
 
-        return orderService.save(order);
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN,
+                "Sorry, you are not owner of this car"));
     }
 }
