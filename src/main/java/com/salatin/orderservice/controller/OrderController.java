061f8f56b521652359f3dfcf574fc256297ada0c
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-03 21:07:43 +0300

    in OrderController method cancel() created and access restricted on create and cancel mthds

--- src/main/java/com/salatin/orderservice/controller/OrderController.java
+++ src/main/java/com/salatin/orderservice/controller/OrderController.java
@@ -7,8 +7,11 @@ import com.salatin.orderservice.service.OrderRegistrationService;
 import com.salatin.orderservice.service.mapper.OrderMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +26,20 @@ public class OrderController {
     private final OrderMapper orderMapper;
 
     @PostMapping
+    @PreAuthorize(value = "hasAnyRole('manager', 'customer')")
     public Mono<OrderResponseDto> create(@RequestBody @Valid OrderCreateRequestDto requestDto,
                                          @AuthenticationPrincipal JwtAuthenticationToken authenticationToken) {
         Order order = orderMapper.toModel(requestDto);
 
         return orderRegistrationService.register(order, authenticationToken)
                 .map(orderMapper::toDto);
+    }
+
+    @PatchMapping("/{orderId}/cancel")
+    @PreAuthorize(value = "hasAnyRole('manager', 'customer')")
+    public Mono<OrderResponseDto> cancel(@PathVariable(value = "orderId") String orderId,
+                                         @AuthenticationPrincipal JwtAuthenticationToken authenticationToken) {
+
+        return null;
     }
 }
