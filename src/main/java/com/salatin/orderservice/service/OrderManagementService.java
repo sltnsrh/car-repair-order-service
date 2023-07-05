aafcb85f97790a35b2bbe9e7cf485da258843570
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-05 20:32:28 +0300

    OrderManagementService class readability was impoved

--- src/main/java/com/salatin/orderservice/service/OrderManagementService.java
+++ src/main/java/com/salatin/orderservice/service/OrderManagementService.java
@@ -3,7 +3,6 @@ package com.salatin.orderservice.service;
 import com.salatin.orderservice.model.Car;
 import com.salatin.orderservice.model.Order;
 import com.salatin.orderservice.model.OrderStatus;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -23,63 +22,30 @@ public class OrderManagementService {
     private final OrderService orderService;
     private final WebClient.Builder webClientBuilder;
 
-    public Mono<Order> create(@NotNull Order order,
-                              @NotNull JwtAuthenticationToken authentication) {
+    public Mono<Order> create(Order order, JwtAuthenticationToken authentication) {
         var bearerToken = "Bearer " + authentication.getToken().getTokenValue();
         var carId = order.getCarId();
 
         return checkIfCarHasNotOpenedOrders(carId)
-            .then(webClientBuilder.build()
-                .get()
-                .uri("http://car-service/cars/{carId}", carId)
-                .header(HttpHeaders.AUTHORIZATION, bearerToken)
-                .retrieve()
-                .onStatus(HttpStatusCode::is5xxServerError,
-                    e -> Mono.error(new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE,
-                        "Car-service temporary unavailable")))
-                .bodyToMono(Car.class)
-                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    "Can't find a car with id: " + carId)))
-                .doOnNext(car -> log.info("Retrieved the car: {}", car))
-                .map(Car::getOwnerId)
-                .flatMap(ownerId -> {
-                    order.setCustomerId(ownerId);
-
-                    if (hasRoleManager(authentication)) {
-                        return registerAsManager(order, authentication.getName());
-                    } else {
-                        return registerAsCustomer(order, authentication.getName());
-                    }
-                }));
+            .then(getCarFromCarService(carId, bearerToken))
+            .flatMap(car -> registerOrder(order, car, authentication));
     }
 
-    public Mono<Order> cancel(String orderId,
-                                         JwtAuthenticationToken authenticationToken) {
-        return orderService.findById(orderId)
+
+    public Mono<Order> cancel(String orderId, JwtAuthenticationToken authenticationToken) {
+        return findByIdOrError(orderId)
             .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
                 "Can't find an order by id: " + orderId)))
             .doOnNext(order -> log.info("Retrieved the order: {}", order))
-            .flatMap(order -> {
-                var orderStatus = order.getStatus();
-
-                if (clientIsManagerAndOrderStatusUnacceptable(authenticationToken, orderStatus)
-                || clientIsCustomerAndOrderStatusUnacceptable(authenticationToken, orderStatus)) {
-
-                    return Mono.error(new ResponseStatusException(HttpStatus.ACCEPTED,
-                        "You can't cancel the order, because it is already " + order.getStatus()));
-                }
-                order.setStatus(OrderStatus.CANCELED);
-                log.info("Order {} status was changed to {} by the user {}",
-                    order.getId(), order.getStatus(), authenticationToken.getName());
-
-                return orderService.save(order).log();
-            });
+            .flatMap(order -> processCancellation(order, authenticationToken))
+            .doOnNext(order -> log.info("Order {} status was changed to {} by the user {}",
+                order.getId(), order.getStatus(), authenticationToken.getName()))
+            .flatMap(orderService::save)
+            .log();
     }
 
     public Mono<Order> getById(String orderId) {
-        return orderService.findById(orderId)
-            .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Can't find an order by id: " + orderId)));
+        return findByIdOrError(orderId);
     }
 
     private Mono<Void> checkIfCarHasNotOpenedOrders(String carId) {
@@ -95,6 +61,31 @@ public class OrderManagementService {
                             "This car is already in the process of repairing"));
                 }
             });
+    }
+
+    private Mono<Car> getCarFromCarService(String carId, String bearerToken) {
+        return webClientBuilder.build()
+            .get()
+            .uri("http://car-service/cars/{carId}", carId)
+            .header(HttpHeaders.AUTHORIZATION, bearerToken)
+            .retrieve()
+            .onStatus(HttpStatusCode::is5xxServerError,
+                response -> Mono.error(new ResponseStatusException(
+                HttpStatus.SERVICE_UNAVAILABLE, "Car service temporarily unavailable")))
+            .bodyToMono(Car.class)
+            .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Can't find a car with id: " + carId)))
+            .doOnNext(car -> log.info("Retrieved the car: {}", car));
+    }
+
+    private Mono<Order> registerOrder(Order order, Car car, JwtAuthenticationToken authentication) {
+        order.setCustomerId(car.getOwnerId());
+
+        if (hasRoleManager(authentication)) {
+            return registerAsManager(order, authentication.getName());
+        } else {
+            return registerAsCustomer(order, authentication.getName());
+        }
     }
 
     private boolean hasRoleManager(JwtAuthenticationToken authenticationToken) {
@@ -118,6 +109,31 @@ public class OrderManagementService {
 
         return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN,
                 "Sorry, you are not owner of this car"));
+    }
+
+    private Mono<Order> findByIdOrError(String orderId) {
+        return orderService.findById(orderId)
+            .switchIfEmpty(Mono.error(() -> createOrderNotFoundException(orderId)));
+    }
+
+    private ResponseStatusException createOrderNotFoundException(String orderId) {
+        return new ResponseStatusException(HttpStatus.NOT_FOUND,
+            "Can't find an order with id: " + orderId);
+    }
+
+    private Mono<Order> processCancellation(Order order,
+                                            JwtAuthenticationToken authenticationToken) {
+        OrderStatus orderStatus = order.getStatus();
+
+        if (clientIsManagerAndOrderStatusUnacceptable(authenticationToken, orderStatus)
+            || clientIsCustomerAndOrderStatusUnacceptable(authenticationToken, orderStatus)) {
+
+            return Mono.error(new ResponseStatusException(HttpStatus.ACCEPTED,
+                "You can't cancel the order because it is already " + order.getStatus()));
+        }
+
+        order.setStatus(OrderStatus.CANCELED);
+        return Mono.just(order);
     }
 
     private boolean clientIsManagerAndOrderStatusUnacceptable(JwtAuthenticationToken authenticationToken,
