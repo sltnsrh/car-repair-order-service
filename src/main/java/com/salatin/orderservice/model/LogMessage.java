186df2449b26d6c2f43bb5585af807a5c3147437
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-11 21:23:57 +0300

    added 'time' field to the LogMessage

--- src/main/java/com/salatin/orderservice/model/LogMessage.java
+++ src/main/java/com/salatin/orderservice/model/LogMessage.java
@@ -1,7 +1,10 @@
 package com.salatin.orderservice.model;
 
+import java.time.LocalDateTime;
+
 public class LogMessage {
     private String id;
     private String from;
     private String text;
+    private LocalDateTime time;
 }
